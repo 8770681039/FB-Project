28ac79956e82d8ebadfdc46ba8b688c9c8ce8305
DELL <DELL@DESKTOP-QOSAG9I>
2023-11-18 12:46:48 +0530

    Contect Page Code

---
+++ src/main/java/BaseLayer/ContectPage.java
@@ -0,0 +1,14 @@
+package BaseLayer;
+
+
+
+public class ContectPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is contect page");
+		
+
+	}
+
+}
